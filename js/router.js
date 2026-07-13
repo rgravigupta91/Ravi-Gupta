80986852ae347071e87a8a6afc3022635c25f505
rg.ravigupta91 <rg.ravigupta91@gmail.com>
2026-07-14 04:31:30 +0530

    July 14, 2026 - index page corrections

--- js/router.js
+++ js/router.js
@@ -1,9 +1,8 @@
 async function loadView(view){
-
+    
     const response = await fetch(`views/${view}.html`);
 
     const html = await response.text();
-
     document.getElementById("content").innerHTML = html;
 
 }
---
+++ views/journal.html
@@ -0,0 +1,1 @@
+<div>Journal</div>
